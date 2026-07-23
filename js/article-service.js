bdfbe1c428db70a59237277ba1645cdd881a8845
worldsolidaryart-cmyk <worldsolidaryart@gmail.com>
2026-07-22 21:34:06 -0300

    Update article-service.js

--- js/article-service.js
+++ js/article-service.js
@@ -1,38 +1,52 @@
 class ArticleService {
 
-    static async getTodos() {
+    static artigos = null;
+
+    static async carregar() {
+
+        if (this.artigos) return this.artigos;
 
         const resposta = await fetch("data/articles.json");
 
-        const artigos = await resposta.json();
+        this.artigos = await resposta.json();
+
+        return this.artigos;
+
+    }
+
+    static async getTodos() {
+
+        const artigos = await this.carregar();
 
         return artigos
-            .filter(artigo => artigo.publicado)
-            .sort((a, b) => new Date(b.data) - new Date(a.data));
+            .filter(a => a.publicado)
+            .sort((a,b)=>new Date(b.data)-new Date(a.data));
 
     }
 
-    static async getRecentes(qtd = 2) {
+    static async getRecentes(qtd=2){
 
         const artigos = await this.getTodos();
 
-        return artigos.slice(0, qtd);
+        return artigos.slice(0,qtd);
 
     }
 
-    static async getPorId(id) {
+    static async getPorId(id){
 
         const artigos = await this.getTodos();
 
-        return artigos.find(a => a.id === id);
+        return artigos.find(a=>a.id===id);
 
     }
 
-    static async getCategorias() {
+    static async getCategorias(){
 
         const artigos = await this.getTodos();
 
-        return [...new Set(artigos.map(a => a.categoria))];
+        return [...new Set(
+            artigos.map(a=>a.categoria)
+        )].sort();
 
     }
 
